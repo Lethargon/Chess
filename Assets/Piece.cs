0ae9290a0c93cf6d8d52e0d022e98cffd2e5e695
Lethargon <jonathan@eldfluga.se>
2024-09-29 10:57:18 +0200

    Preliminary data structures

---
+++ Assets/Piece.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Piece : MonoBehaviour
+{
+    Coords pos;
+    public int id;
+    Coords[] legalMoves;
+    Color color;
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    Coords pos;
+    bool empty = true;
+    public GameObject piece;
+}
